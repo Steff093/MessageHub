1d176bc4ccf2fc898c86b5abcf92d7d314a8e50b
Steff <skaterkeks@googlemail.com>
2024-07-14 16:13:00 +0200

    MES-1-Add Server Project

---
+++ MessageHub.Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using Network;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MessageHub.Server
+{
+    // All the code in this file is included in all platforms.
+    public class Program
+    {
+        static TcpListener _listener {  get; set; }
+        static void Main(string[] args)
+        {
+            _listener = new TcpListener(IPAddress.Parse("127.0.01", "7000"));
+            _listener.Start();
+
+            var client = _listener.AcceptTcpClient();
+        }
+    }
+}
