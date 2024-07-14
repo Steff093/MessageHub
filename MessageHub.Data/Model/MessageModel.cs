26f0310f19aa64e6a7072e0a6f1d5a4afe412b1b
Steff <skaterkeks@googlemail.com>
2024-07-14 15:42:04 +0200

    MES-1-Create Project

---
+++ MessageHub.Data/Model/MessageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MessageHub.Data.Model
+{
+    public class MessageModel
+    {
+        public string UserName { get; set; }
+        public string? Sender { get; set; }
+        public string? Content { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ MessageHub/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MessageHub
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
